63ea1355e3aa01ce9b0031830d76d8bb6e11c595
fipiaggiodeb <fipiaggio@debmedia.com>
2018-10-08 13:14:25 -0300

    * Agrego middleware que mira si estás logeado para entrar a crear una nueva receta. Si no es así te lleva al login
    * Agrego método para dar la vista de creación

--- app/Http/Controllers/RecetasController.php
+++ app/Http/Controllers/RecetasController.php
@@ -7,13 +7,36 @@ use \App\Recipe;
 
 class RecetasController extends Controller
 {
+    /**
+     * Verifica con Auth en algunos métodos
+     */
+    public function __construct()
+    {
+        $this->middleware('auth', ['only' => ['create']]);
+    }
+
+    /**
+     * Vista general de recetas
+     * Lista todas
+     */
     public function index(){
         $recipes = Recipe::all();
         return view('web.recetas')->with('recipes', $recipes);
     }
 
+    /**
+     * Vista particular de recetas
+     * Muestra una sola si existe, sino tira 404
+     */
     public function show($id){
         $recipe = Recipe::findOrFail($id);
         return view('web.receta')->with('recipe', $recipe);
+    }
+
+    /**
+     * Creación de una receta
+     */
+    public function create(){
+        return view('user.crearReceta');
     }
 }
