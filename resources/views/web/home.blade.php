4b00512927c82e04c1d25c6f2d344a7baf8c1386
fipiaggiodeb <fipiaggio@debmedia.com>
2018-10-08 13:13:38 -0300

    Agrego link a nueva receta

--- resources/views/web/home.blade.php
+++ resources/views/web/home.blade.php
@@ -24,21 +24,25 @@
             </li>
         @else
             <li class="nav-item dropdown">
-                <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
-                    {{ Auth::user()->name }} <span class="caret"></span>
+                Nombre de usuario: {{ Auth::user()->name }} <span class="caret"></span>
+            </li>
+            <li>
+                <a href="recetas/nueva">Crear receta nueva</a>
+            </li>
+            @if(Auth::user()->hasRole('admin'))
+                <li>
+                    <a href="/admin">Vista que solo pueden ver los admin</a>                    
+                </li>
+            @endif
+            <li>
+                <a 
+                class="dropdown-item" href="{{ route('logout') }}"
+                onclick="event.preventDefault(); document.getElementById('logout-form').submit();">
+                    {{ __('Logout') }}
                 </a>
-
-                <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
-                    <a 
-                    class="dropdown-item" href="{{ route('logout') }}"
-                    onclick="event.preventDefault(); document.getElementById('logout-form').submit();">
-                        {{ __('Logout') }}
-                    </a>
-
-                    <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
-                        @csrf
-                    </form>
-                </div>
+                <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
+                    @csrf
+                </form>
             </li>
         @endguest
     </ul>
