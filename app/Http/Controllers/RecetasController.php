345d45ee258be27311d211930dd762d8e138a70d
fipiaggiodeb <fipiaggio@debmedia.com>
2018-10-08 12:02:07 -0300

    Actualizo el controller de la vista de recetas. Lista una particular o lista todas

--- app/Http/Controllers/RecetasController.php
+++ app/Http/Controllers/RecetasController.php
@@ -3,10 +3,17 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use \App\Recipe;
 
 class RecetasController extends Controller
 {
     public function index(){
-        return view('recetas');
+        $recipes = Recipe::all();
+        return view('web.recetas')->with('recipes', $recipes);
+    }
+
+    public function show($id){
+        $recipe = Recipe::findOrFail($id);
+        return view('web.receta')->with('recipe', $recipe);
     }
 }
