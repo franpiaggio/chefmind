47088b02f59428d448a568938e1c4422523041a0
fipiaggiodeb <fipiaggio@debmedia.com>
2018-10-07 21:30:50 -0300

    Asigno rol por defecto en el registro de usuario

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -63,10 +63,16 @@ class RegisterController extends Controller
      */
     protected function create(array $data)
     {
-        return User::create([
+        // Registro de nuevo usuario
+        $user = User::create([
             'name' => $data['name'],
             'email' => $data['email'],
-            'password' => Hash::make($data['password']),
+            'password' => bcrypt($data['password']),
         ]);
+        // Asignación de rol "usuario" por defecto
+        $user
+            ->roles()
+            ->attach(Role::where('name', 'user')->first());
+        return $user;
     }
 }
