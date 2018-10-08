4fcf24a04c57ca553774910eb069c62affa90ea1
fipiaggiodeb <fipiaggio@debmedia.com>
2018-10-08 17:41:44 -0300

    Validado de creación de receta

--- app/Http/Controllers/RecetasController.php
+++ app/Http/Controllers/RecetasController.php
@@ -2,41 +2,68 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
+use Request;
 use \App\Recipe;
+use Carbon\Carbon;
+use App\Http\Requests\CreateRecipeRequest;
 
 class RecetasController extends Controller
 {
     /**
      * Verifica con Auth en algunos métodos
      */
-    public function __construct()
-    {
-        $this->middleware('auth', ['only' => ['create']]);
+    public function __construct(){
+        $this->middleware('auth', ['only' => ['create', 'store']]);
     }
 
     /**
      * Vista general de recetas
      * Lista todas
+     * @return Response
      */
     public function index(){
-        $recipes = Recipe::all();
-        return view('web.recetas')->with('recipes', $recipes);
+        $recipes = Recipe::latest()->get();
+        return view('web.recetas', compact('recipes'));
     }
 
     /**
      * Vista particular de recetas
      * Muestra una sola si existe, sino tira 404
+     * @return Response
      */
     public function show($id){
         $recipe = Recipe::findOrFail($id);
-        return view('web.receta')->with('recipe', $recipe);
+        return view('web.receta', compact('recipe'));
     }
 
     /**
-     * Creación de una receta
+     * Vista de creación de una receta
+     * @return Response
      */
     public function create(){
         return view('user.crearReceta');
+    }
+
+    /**
+     * Guarda una receta en la DB
+     * @param CreateArticleRequest $request
+     * @return Response
+     */
+    public function store(CreateRecipeRequest $request){
+        // Obtengo todo lo que viene de la Request
+        $input = $request->all();
+        // Seteo la fecha
+        $input['published_at'] = Carbon::now(); 
+        // Guardo en DB la nueva receta
+        Recipe::create($input);
+        // Vuelvo a ver las recetas
+        return redirect('recetas');
+    }
+
+    /**
+     * Edita una receta
+     */
+    public function edit(){
+        
     }
 }
