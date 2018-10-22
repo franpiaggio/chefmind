508c3ae025e46b32168ce44b55b5830084bd0fda
fpiaggio <fpiaggio92@gmail.com>
2018-10-21 21:12:46 -0300

    Model categoria.

--- app/Category.php
+++ app/Category.php
@@ -6,5 +6,10 @@ use Illuminate\Database\Eloquent\Model;
 
 class Category extends Model
 {
-    //
+    /**
+     * Asocio Recetas y categorias
+     */
+    public function recipes(){
+        return $this->belongsToMany('App\Category')->withTimestamps();
+    }
 }
