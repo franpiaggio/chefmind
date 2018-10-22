006a7a0ebbd6367fb518f0724c41c6e316edda79
fpiaggio <fpiaggio92@gmail.com>
2018-10-22 16:33:23 -0300

    Dejo visible el boton de logout

--- resources/views/admin/admin.blade.php
+++ resources/views/admin/admin.blade.php
@@ -28,16 +28,14 @@
                 Nombre de usuario: {{ Auth::user()->name }} <span class="caret"></span>
             </li>
             <li>
-                <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
-                    <a 
-                    class="dropdown-item" href="{{ route('logout') }}"
-                    onclick="event.preventDefault(); document.getElementById('logout-form').submit();">
-                        {{ __('Logout') }}
-                    </a>
-                    <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
-                        @csrf
-                    </form>
-                </div>
+                <a 
+                href="{{ route('logout') }}"
+                onclick="event.preventDefault(); document.getElementById('logout-form').submit();">
+                    {{ __('Logout') }}
+                </a>
+                <form id="logout-form" action="{{ route('logout') }}" method="POST">
+                    @csrf
+                </form>
             </li>
         @endguest
     </ul>
