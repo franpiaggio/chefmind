8913e01c2bf883866e55fc49e48de3218faab06b
fpiaggio <fipiaggiodev@gmail.com>
2018-11-11 20:32:01 -0300

    Busqueda por ingredientes y recetas que los contengan

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -4,6 +4,8 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
+use App\Recipe;
+use App\Ingredient;
 
 class HomeController extends Controller
 {
@@ -12,5 +14,28 @@ class HomeController extends Controller
      */
     public function index(Request $request){
         return view('web.home');
+    }
+
+    /**
+     * Buscador
+     */
+    public function search(Request $request){
+        // Nombres ingresados
+        $names = $request->all()['ingredients'];
+        // Obtengo los ingredientes
+        $ingredients = Ingredient::whereIn('name', $names)->get();
+        // Separo solo los ids para a consulta
+        $ids = [];
+        foreach($ingredients as $ingredient){
+            $ids[] = $ingredient->id;  
+        }
+        // Busco las recetas que como mínimo tengan los ingredientes solicitados
+        $recipes = Recipe::whereHas('ingredients', function($q) use($ids) {
+            $q->whereIn('ingredient_id', $ids)
+            ->groupBy('recipe_id')
+            ->havingRaw('COUNT(DISTINCT ingredient_id) = '.count($ids));
+        })->paginate(10);
+        // Devuelvo la vista con las recetas paginadas
+        return view('web.recetas', compact('recipes'));
     }
 }
