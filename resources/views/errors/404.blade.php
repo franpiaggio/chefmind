1dfb16748aa2e00837ed604716909df542f98081
fpiaggio92@gmail.com <fpiaggio92@gmail.com>
2018-12-05 03:42:02 -0300

    errores ratings

--- resources/views/errors/404.blade.php
+++ resources/views/errors/404.blade.php
@@ -5,8 +5,8 @@
     <div class="container-fluid errors d-flex">
         <div class="container d-flex flex-column align-items-center justify-content-center">
             <i class="fas fa-exclamation-circle mb-3"></i>
-            <h1 class="error-number">401</h1>
-            <p class="">No tienes permisos para acceder a esta ruta.</p>
+            <h1 class="error-number">404</h1>
+            <p class="">El link al que intentas acceder no existe.</p>
             <a href="/" class="btn btn-outline-danger">Volver al home</a>
         </div>
     </div>
