6588f90be349904b9d1c781d2c371b792156ac2d
fpiaggio92@gmail.com <fpiaggio92@gmail.com>
2018-12-04 23:52:20 -0300

    Vista de receta

--- resources/views/web/home.blade.php
+++ resources/views/web/home.blade.php
@@ -8,7 +8,7 @@
             <p class="lead text-center search-desc">¿Tenés los ingredientes pero no sabés qué cocinar? <br /> <span>Escribilos en este buscador</span> y vas a poder resolverlo!</p>          
             <form class="mb-2 mt-4" method="GET" action="/buscar">
                 <div class="form-row">
-                    <div class="col-12 col-md-8 offset-md-1 mb-2 mb-md-0 search-input">
+                    <div class="col-12 col-md-8 offset-md-2 mb-2 mb-md-0 search-input">
                         <select id="ingredientsSelector" name="ingredients[]" class="form-control form-control-lg js-ingredients d-none" multiple>
                         </select>
                     </div>
@@ -17,7 +17,7 @@
                     </div>
                 </div>
             </form>
-            <small class="offset-md-1">¿Buscás una receta en especial?<a href="/categorias" class="ml-1">Ingresá aquí</a></small>
+            <small class="offset-md-2">¿Buscás una receta en especial?<a href="/categorias" class="ml-1">Ingresá aquí</a></small>
         </div>
     </section>
     <section class="container mb-5">
